6e582a37435fed9404fbb68a75c00416edc0d84f
thumminbird14 <tanyapurcell@hotmail.co.uk>
2022-03-11 17:03:23 +0000

    add html templates

--- controllers/controller.py
+++ controllers/controller.py
@@ -9,8 +9,5 @@ def choices(choice_1, choice_2):
     player_2 = Player("TestPlayer2", choice_2)
     this_game = Game(player_1, player_2)
     winner = Game.play_game(this_game)
-    return f"Choice 1 is {choice_1}, choice 2 is {choice_2}, winner is {winner}"
-
-
-# def index():
-    # return render_template('index.html', title='Home')
+    # return f"Choice 1 is {choice_1}, choice 2 is {choice_2}, winner is {winner}"
+    return render_template("index.html", choice_1=choice_1, choice_2=choice_2, winner=winner)
