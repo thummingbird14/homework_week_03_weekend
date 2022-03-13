732d9cbafd0a378f13971c3aaae51214c788d63c
thumminbird14 <tanyapurcell@hotmail.co.uk>
2022-03-13 11:15:16 +0000

    changes to display player names

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,13 +1,12 @@
-from flask import render_template, request
+from flask import render_template
 from app import app
 from models.game import Game
 from models.player import Player
 
 @app.route('/<choice_1>/<choice_2>')
 def choices(choice_1, choice_2):
-    player_1 = Player("TestPlayer1", choice_1)
-    player_2 = Player("TestPlayer2", choice_2)
+    player_1 = Player("Fred", choice_1)
+    player_2 = Player("Bill", choice_2)
     this_game = Game(player_1, player_2)
     winner = Game.play_game(this_game)
-    # return f"Choice 1 is {choice_1}, choice 2 is {choice_2}, winner is {winner}"
-    return render_template("index.html", choice_1=choice_1, choice_2=choice_2, winner=winner)
+    return render_template("index.html", name_1=player_1.name, name_2=player_2.name, choice_1=choice_1, choice_2=choice_2, winner=winner)
