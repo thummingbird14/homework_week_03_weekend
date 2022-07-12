b634fe96ed44268cd8f52c1a0bbe6e2f06453a37
thumminbird14 <tanyapurcell@hotmail.co.uk>
2022-07-12 16:01:04 +0100

    extension - play against computer

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,12 +1,12 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 from models.game import Game
 from models.player import Player
 
 @app.route('/<choice_1>/<choice_2>')
 def choices(choice_1, choice_2):
-    player_1 = Player("Fred", choice_1)
-    player_2 = Player("Bill", choice_2)
+    player_1 = Player("Player1", choice_1)
+    player_2 = Player("Player2", choice_2)
     this_game = Game(player_1, player_2)
     winner = Game.play_game(this_game)
     return render_template("index.html", name_1=player_1.name, name_2=player_2.name, choice_1=choice_1, choice_2=choice_2, winner=winner)
@@ -14,3 +14,20 @@ def choices(choice_1, choice_2):
 @app.route('/welcome')
 def welcome():
     return render_template("welcome.html")
+
+@app.route('/play')
+def play():
+    return render_template("play.html")
+
+@app.route('/play', methods=['POST'])
+def random_choice():
+    name_1 = request.form['username']
+    choice_1 = request.form['move']
+    name_2 = 'Computer'
+    choice_2 = Game.make_random_choice()
+    player_1 = Player(name_1, choice_1)
+    player_2 = Player(name_2, choice_2)
+    that_game = Game(player_1, player_2)
+    winner = Game.play_game(that_game)
+    return render_template('index.html', name_1=player_1.name, name_2=player_2.name, choice_1=choice_1, choice_2=choice_2, winner=winner)
+    
--- models/game.py
+++ models/game.py
@@ -1,3 +1,5 @@
+import random
+
 class Game():
     def __init__(self, player_1, player_2):
         self.player_1 = player_1
@@ -5,7 +7,7 @@ class Game():
 
     def play_game(self):
         if self.player_1.choice == self.player_2.choice:
-            return None
+            return "Draw"
         if self.player_1.choice == "rock" and self.player_2.choice == "scissors":
             return self.player_1.name
         elif self.player_1.choice == "paper" and self.player_2.choice == "rock":
@@ -14,4 +16,9 @@ class Game():
             return self.player_1.name
         else:
             return self.player_2.name
+
+    def make_random_choice():
+        moves = ["rock", "paper", "scissors"]
+        choice = random.choice(moves)
+        return choice
 
